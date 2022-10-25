4b755bf9c369134c683d4a23a327b7ba245a5fcb
LeeHuynSuk <gusehf321@naver.com>
2022-10-25 17:05:50 +0900

    [delete] redux-toolkit

--- src/CardBox.js
+++ src/CardBox.js
@@ -1,5 +1,5 @@
-/* // REACT QUERY 이용해서 오픈 API 읽어오기 성공!
-import React from "react";
+// REACT QUERY 이용해서 오픈 API 읽어오기 성공!
+import React, { useEffect } from "react";
 import { Button, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styled from "styled-components";
@@ -7,7 +7,7 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const getPokeList = () => {
-  return axios.get("https://pokeapi.co/api/v2/pokemon?limit=1153&offset=0");
+  return axios.get("https://pokeapi.co/api/v2/pokemon?limit=153&offset=0");
 };
 
 const CardBox = () => {
@@ -17,16 +17,19 @@ const CardBox = () => {
     },
   });
 
-  console.log(poke_query.data?.data?.results);
+  useEffect(() => {
+    console.log(poke_query.data?.data?.results);
+  }, [poke_query.data?.data?.results]);
+
   return (
     <Cards>
-      {poke_query.data?.data?.results.map((d, i) => {
+      {poke_query.data?.data?.results.map((d, index) => {
         return (
-          <Card style={{ width: "15rem", margin: "1rem" }} key={d.id}>
+          <Card style={{ width: "15rem", margin: "1rem" }} key={d.card}>
             <Card.Img
               variant="top"
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                i + 1
+                index + 1
               }.png`}
             />
             <Card.Body>
@@ -65,62 +68,6 @@ const card = styled.div`
   position: relative;
   overflow: hidden;
   margin-bottom: 2%;
-`;
-
-export default CardBox; */
-
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Button, Card } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import styled from "styled-components";
-import { getCardsThunk } from "./redux/modules/cardSlice";
-
-const CardBox = () => {
-  const cardList = useSelector((state) => state.cardSlice.data.results);
-  console.log(cardList);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getCardsThunk());
-  }, []);
-
-  useEffect(() => {
-    console.log(cardList);
-  }, [cardList]);
-  return (
-    <Cards>
-      {cardList.map((data, index) => {
-        return (
-          <Card style={{ width: "15rem", margin: "1rem" }} key={index}>
-            <Card.Img
-              variant="top"
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                index + 1
-              }.png`}
-            />
-            <Card.Body>
-              <Card.Title>{data.name}</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="outline-warning">상세 내용</Button>
-            </Card.Body>
-          </Card>
-        );
-      })}
-    </Cards>
-  );
-};
-
-const Cards = styled.div`
-  width: 100%;
-  max-width: 5000px;
-  gap: 2%;
-  display: flex;
-  justify-content: flex-start;
-  flex-wrap: wrap;
 `;
 
 export default CardBox;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* // REACT QUERY 전용 index.
+// REACT QUERY 전용 index.
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -20,21 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(); */
-
-// REDUX TOOLKIT 전용 index.
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import store from "./redux/configStore";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
 reportWebVitals();
